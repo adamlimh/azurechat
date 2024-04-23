23b07f0dfafcf5cb41ca5699707e8f7d82e3dcac
adamlimh <adamlimh@gmail.com>
2024-04-23 11:36:56 +0800

    Update chat-message-empty-state.tsx

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -17,10 +17,10 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
     <div className="grid grid-cols-5 w-full items-center container mx-auto max-w-3xl justify-center h-full gap-9">
       <div className="col-span-2 gap-5 flex flex-col flex-1">
         <img src="/ai-icon.png" className="w-36" />
-        <p className=""><b style="color:red;">
+        <p className="text-red-500 font-bold">
           !!What's news: We've upgraded to GPT-4 Turbo, now with 128k tokens
           —10x our previous capacity, equal to 300 pages!!
-        </b></p>
+        </p>
       </div>
       <Card className="col-span-3 flex flex-col gap-5 p-5 ">
         <Typography variant="h4" className="text-primary">
