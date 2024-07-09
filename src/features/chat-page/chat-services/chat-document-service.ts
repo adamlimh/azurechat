7b1b218b79eb9531ebe245f5a86ad2f741d60724
adamlimh <adamlimh@gmail.com>
2024-07-09 17:49:08 +0800

    Update chat-document-service.ts

--- src/features/chat-page/chat-services/chat-document-service.ts
+++ src/features/chat-page/chat-services/chat-document-service.ts
@@ -13,9 +13,9 @@ import { EnsureIndexIsCreated } from "./azure-ai-search/azure-ai-search";
 import { CHAT_DOCUMENT_ATTRIBUTE, ChatDocumentModel } from "./models";
 
 const MAX_UPLOAD_DOCUMENT_SIZE: number = 20000000;
-const CHUNK_SIZE = 2300;
-// 25% overlap
-const CHUNK_OVERLAP = CHUNK_SIZE * 0.25;
+const CHUNK_SIZE = 8000;
+// 20% overlap
+const CHUNK_OVERLAP = CHUNK_SIZE * 0.2;
 
 export const CrackDocument = async (
   formData: FormData
